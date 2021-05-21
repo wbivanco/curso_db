7e87f38468210e06d4af6e81cae39c3e14855765
Walter Bivanco <walterbivanco@gmail.com>
2021-05-21 17:10:50 -0300

    Clase 5 - Variables de entorno

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pymysql
+from decouple import config
 
 DROP_TABLE_USERS = "DROP TABLE IF EXISTS users"
 
@@ -11,9 +12,20 @@ USERS_TABLE = """ CREATE TABLE users(
 ) """
 
 
+""" 
+Creo variables de entorno con la consola del sistema operativo escribiendo:
+export USER_MYSQL=root
+export PASSWORD_MYSQL=
+export DB_MYSQL=pythondb
+"""
+
 if __name__ == '__main__':
     try:
-        connect = pymysql.Connect(host='localhost', port=3306, user='root', passwd='', db='pythondb')
+        connect = pymysql.Connect(host='localhost',
+                                  port=3306,
+                                  user=config('USER_MYSQL'),
+                                  passwd=config('PASSWORD_MYSQL'),
+                                  db=config('DB_MYSQL'))
 
         with connect.cursor() as cursor:
             cursor.execute(DROP_TABLE_USERS)
