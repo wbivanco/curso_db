aa9e9bf000e9865cf00b3b692205de49f38a224b
Walter Bivanco <walterbivanco@gmail.com>
2021-05-23 16:57:36 -0300

    Proyecto 1 - Clase 2 - Ciclo while

--- proyecto1.py
+++ proyecto1.py
@@ -9,7 +9,37 @@ USERS_TABLE = """ CREATE TABLE users(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )"""
 
+def create_user():
+    """ A) Crear Usario"""
+
+    print("Usuario creado")
+
+def list_users():
+    """ B) Listar usuarios"""
+
+    print("Listado de usuarios")
+
+def update_user():
+    """ C) Actualizar Usuario"""
+
+    print("Usuario modificado")
+
+def delete_user():
+    """ D) Eliminar usuario"""
+
+    print("Usuario eliminado")
+
+def default():
+    print("Opción no valida!")
+
 if __name__ == '__main__':
+    options = {
+        'a': create_user,
+        'b': list_users,
+        'c': update_user,
+        'd': delete_user
+    }
+
     try:
         connect = psycopg2.connect("postgresql://postgres:12345678@127.0.0.1/curso_bd")
 
@@ -17,6 +47,20 @@ if __name__ == '__main__':
             cursor.execute(DROP_USERS_TABLE)
             cursor.execute(USERS_TABLE)
             connect.commit()
+
+            while True:
+                for function in options.values():
+                    print(function.__doc__)
+
+                print("quit para salir")
+
+                option = input("Seleccione una opción válida: ").lower()
+
+                if option == "quit" or option == "q":
+                    break
+
+                function = options.get(option, default)
+                function()
 
     except psycopg2.OperationalError as err:
         print("No fue posible realizar la conexion")
